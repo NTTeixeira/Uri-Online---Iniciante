63efa3599374229e421b9843d450e129ccdafba3
NTTeixeira <64165617+NTTeixeira@users.noreply.github.com>
2021-04-14 13:50:18 -0300

    Create 1005 - Média 1.java

---
+++ Iniciante/1005 - Média 1.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program {
+
+	public static void main(String[] args) {
+			
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		
+		double media = ((a * 3.5) + (b * 7.5)) / 11;
+		
+		System.out.printf("MEDIA = %.5f", media);
+		
+		sc.close();
+	}
+
+}
